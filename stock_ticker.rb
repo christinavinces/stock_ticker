bd17372c8e1295baba36bf5cd9985deaca89aa96
christinavinces <christina.vinces@gmail.com>
2015-08-14 22:12:26 -0400

    Update stock_ticker.rb

--- stock_ticker.rb
+++ stock_ticker.rb
@@ -12,5 +12,5 @@ my_span = dom.xpath("//span[@id='yfs_l84_"+ ticker +"']").first
 prev_close = dom.xpath("//td[@class='yfnc_tabledata1']").first
 
 get '/' do
-	["Stock: #{ticker.upcase}", "Current Price: $#{my_span.content}", "Previous Close: $#{prev_close.content}"].join("<br>")
+  ["Stock: #{ticker.upcase}", "Current Price: $#{my_span.content}", "Previous Close: $#{prev_close.content}"].join("<br>")
 end
